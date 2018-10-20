821199e4c1c640bf5de9a2a31a6d6502222b63f2
ericafenyo <ericafenyo@yahoo.com>
2018-10-20 16:36:24 +0200

    update comments

--- app/src/main/java/com/ericafenyo/eyenight/EyeNightViewModel.kt
+++ app/src/main/java/com/ericafenyo/eyenight/EyeNightViewModel.kt
@@ -18,7 +18,6 @@ package com.ericafenyo.eyenight
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
-import com.ericafenyo.eyenight.Injection
 import com.ericafenyo.eyenight.model.NetworkState
 import com.ericafenyo.eyenight.model.UserEntity
 import com.parse.ParseFile
@@ -29,40 +28,46 @@ import com.parse.ParseFile
  */
 class EyeNightViewModel : ViewModel() {
     private val repository = Injection.provideRepository()
-    private val results = repository.getProducts()
-
-    /** Get products from the Parse database
-     * NOTE: This [networkState] belongs to the [products]*/
-    val products = results.data
-    val networkState = results.networkState
+    /**
+     * Returns all Products from the Parse database.
+     */
+    val products = repository.getProducts()
 
     /**
      * Adds a profile image to the currently logged in user.
      * This will fail if there is no authenticated user.
      *
-     * @return  A [LiveData] of [NetworkState]
-     * For example : Success state and Error state
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
      */
     fun addProfileImage(parseFile: ParseFile, objectId: String?) = repository.addProfileImage(parseFile, objectId)
 
     /**
-     * Logs out the currently logged in user.
+     * Deletes the currently logged in user's Section from the Parse database.
+     * Section is a keyword related to Parse server and it represent an instance of a user
+     * logged into a device.
      *
-     * @return  A [LiveData] of [NetworkState]
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     * @see [Parse Section](https://docs.parseplatform.org/android/guide/#sessions)
      */
     fun logOut() = repository.logOut()
 
     /**
-     * Remove the currently logged in user from the Parse database.
+     * Completely remove the currently logged in user from the Parse database.
+     * NOTE: The user can no longer login with the deleted account and any other person can create
+     * an account using the same username and password.
      *
-     * @return  A [LiveData] of [NetworkState]
-     * */
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     */
     fun deleteAccount() = repository.deleteAccount()
 
     /**
      * Logs in a user with a username and password.
      *
-     * @return  A [LiveData] of [NetworkState]
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
      */
     fun attemptLogin(user: UserEntity) = repository.logIn(user)
 
@@ -70,8 +75,8 @@ class EyeNightViewModel : ViewModel() {
     /**
      * Registers a user with a username and password into Parse Database
      *
-     * @return  A [LiveData] of [NetworkState]
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
      */
-
     fun attemptSignUp(user: UserEntity) = repository.signUp(user)
 }
--- app/src/main/java/com/ericafenyo/eyenight/data/DataSource.kt
+++ app/src/main/java/com/ericafenyo/eyenight/data/DataSource.kt
@@ -17,29 +17,76 @@
 package com.ericafenyo.eyenight.data
 
 import android.arch.lifecycle.LiveData
-import com.ericafenyo.eyenight.model.Event
+import com.ericafenyo.eyenight.Result
 import com.ericafenyo.eyenight.model.NetworkState
 import com.ericafenyo.eyenight.model.Product
 import com.ericafenyo.eyenight.model.UserEntity
 import com.parse.ParseFile
 
 /**
- * An object for performing parse database transactions*/
+ * An object for performing parse database transactions
+ */
 interface DataSource {
 
+    /**
+     * Registers a user with a username and password into Parse Database
+     *
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     */
     fun signUp(user: UserEntity): LiveData<NetworkState>
 
+    /**
+     * Logs in a user with a username and password.
+     *
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     */
     fun logIn(user: UserEntity): LiveData<NetworkState>
 
+    /**
+     * Deletes the currently logged in user's Section from the Parse database.
+     * Section is a keyword related to Parse server and it represent an instance of a user
+     * logged into a device.
+     *
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     * @see [Parse Section](https://docs.parseplatform.org/android/guide/#sessions)
+     */
     fun logOut(): LiveData<NetworkState>
 
+    /**
+     * Completely remove the currently logged in user from the Parse database.
+     * NOTE: The user can no longer login with the deleted account and any other person can create
+     * an account using the same username and password.
+     *
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     */
     fun deleteAccount(): LiveData<NetworkState>
 
+    /**
+     * Adds a profile image to the currently logged in user.
+     * This will fail if there is no authenticated user.
+     *
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     */
     fun addProfileImage(parseFile: ParseFile, objectId: String?): LiveData<NetworkState>
 
+    /**
+     * Requests a password reset email to be sent to the specified email address associated with the
+     * user account. This email allows the user to securely reset their password on the Parse site.
+     *
+     * @return  A [LiveData] of [NetworkState]. This NetworkState tracks the loading, success or
+     * error status of the whole process.
+     */
     fun requestPasswordReset(email: String): LiveData<NetworkState>
 
-    fun getProducts(): Listing<List<Product>>
+    /**
+     * Query all Products from the Parse database
+     */
+    fun getProducts(): LiveData<Result<List<Product>>>
 }
 
 data class Listing<T>(val data: LiveData<T>, val networkState: LiveData<NetworkState>)
